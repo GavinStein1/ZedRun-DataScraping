137c58cabf30bac6e85590123769d12d587de58a
Gavin Stein <gste6115@uni.sydney.edu.au>
2023-02-07 22:02:46 +1100

    add write

--- main.py
+++ main.py
@@ -24,33 +24,10 @@ def main():
             data_string = data_string + "," + horse.mother + "," + horse.father
             print(data_string)
             ctr = ctr + 1
+            data_file.write(data_string + "\n")
         except KeyboardInterrupt:
             data_file.close()
             break
-        # try:
-        #     print(ctr)
-        #     horse = scrape.scrape_data(ctr)
-        #     if horse is not None:
-        #         data_string = str(horse.id) + ',' + horse.name + ',' + horse.bloodline + ',' + horse.breed_type + ',' + str(horse.genotype) + ','
-        #         data_string = data_string + stringify_dict(horse.distance1000)
-        #         data_string = data_string + stringify_dict(horse.distance1200)
-        #         data_string = data_string + stringify_dict(horse.distance1400)
-        #         data_string = data_string + stringify_dict(horse.distance1600)
-        #         data_string = data_string + stringify_dict(horse.distance1800)
-        #         data_string = data_string + stringify_dict(horse.distance2000)
-        #         data_string = data_string + stringify_dict(horse.distance2200)
-        #         data_string = data_string + stringify_dict(horse.distance2400)
-        #         data_string = data_string + stringify_dict(horse.distance2600)
-        #         data_string = data_string + stringify_dict(horse.offspring)
-        #         data_string = data_string + horse.mother_name + "," + horse.father_name
-        #         data_file.write(data_string + "\n")
-        #         ctr += 1
-        #     else:
-        #         ctr += 1
-        # except Exception:
-        #     continue
-        # except KeyboardInterrupt:
-        #     data_file.close()
 
 
 
